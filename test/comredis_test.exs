51d44d117c80a813aaffb0c0174e898c0be95053
Iuri Fernandes <iurifq@gmail.com>
2016-03-26 08:14:18 +0100

    Test generation of some simple commands

--- test/comredis_test.exs
+++ test/comredis_test.exs
@@ -2,7 +2,15 @@ defmodule ComredisTest do
   use ExUnit.Case
   doctest Comredis
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "commands without arguments" do
+    assert Comredis.quit == ["QUIT"]
+    assert Comredis.command == ["COMMAND"]
+    assert Comredis.client_list == ["CLIENT LIST"]
+  end
+
+  test "comands without optional arguments" do
+    assert Comredis.get("key") == ~w(GET key)
+    assert Comredis.setnx("key", "value") == ~w(SETNX key value)
+    assert Comredis.cluster_count_failure_reports("node-id") == ["CLUSTER COUNT-FAILURE-REPORTS", "node-id"]
   end
 end
