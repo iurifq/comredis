be44e15c3627474a6c02335f68e864e5871466a0
Iuri Fernandes <iurifq@gmail.com>
2016-03-27 19:30:37 +0200

    Bump version for more documentation adjustments

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Comredis.Mixfile do
 
   def project do
     [app: :comredis,
-     version: "0.0.2",
+     version: "0.0.3",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
