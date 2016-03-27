89f317828f879a6ffd9e77b69ce4f89910814975
Iuri Fernandes <iurifq@gmail.com>
2016-03-27 12:47:07 +0200

    Release to hex.pm

--- mix.exs
+++ mix.exs
@@ -1,12 +1,19 @@
 defmodule Comredis.Mixfile do
   use Mix.Project
 
+  @description """
+  Comredis is your comrade for Redis command generation in Elixir.
+  """
+
   def project do
     [app: :comredis,
      version: "0.0.1",
-     elixir: "~> 1.1",
+     elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     name: "Comredis",
+     description: @description,
+     package: package,
      deps: deps]
   end
 
@@ -16,5 +23,11 @@ defmodule Comredis.Mixfile do
 
   defp deps do
     [{:poison, "~> 2.0"}]
+  end
+
+  defp package do
+    [ maintainers: ["Iuri Fernandes Queiroz"],
+      licenses: ["MIT"],
+      links: %{"Github" => "https://github.com/iurifq/comredis"} ]
   end
 end
