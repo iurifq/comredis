fe83ea1409b1655c190f636160f1d9f60e13f1f1
Iuri Fernandes <iurifq@gmail.com>
2016-03-28 21:49:20 +0200

    Lower the Poison's version requirement

--- mix.exs
+++ mix.exs
@@ -23,7 +23,7 @@ defmodule Comredis.Mixfile do
 
   defp deps do
     [
-      {:poison, "~> 2.0"},
+      {:poison, ">= 1.0.0"},
 
       {:earmark, "~> 0.1", only: :dev},
       {:ex_doc, "~> 0.11", only: :dev},
